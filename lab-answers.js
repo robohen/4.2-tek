34bab3634b7570696eedac8d3cdeb0580530dc6f
robohen <88636103+robohen@users.noreply.github.com>
2023-05-27 16:38:24 -0700

    Wild Wilf Life answered

--- lab-answers.js
+++ lab-answers.js
@@ -11,16 +11,32 @@
 // }
 //
 //Fizz Buzz//
-for(let i = 1; i <= 100; i++){
-    if (i % 3 ===0 && i % 5===0){
-        console.log("FizzBuzz");
-    }else if (i % 3 ===0){
-        console.log("Fizz");
-}
-    else if (i % 5 ===0){
-        console.log("Buzz");
-    }else{
-        console.log(i);
-    }
-}
+// for(let i = 1; i <= 100; i++){
+//     if (i % 3 ===0 && i % 5===0){
+//         console.log("FizzBuzz");
+//     }else if (i % 3 ===0){
+//         console.log("Fizz");
+// }
+//     else if (i % 5 ===0){
+//         console.log("Buzz");
+//     }else{
+//         console.log(i);
+//     }
+// }
+//
+// Wild Wild Life//
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+plantee[2] = 5001;
+console.log(plantee);
+wolfy[3] = "Gotham City";
+console.log(wolfy);
+dart.push("Hawkins");
+console.log(dart);
+wolfy[0] = "Gameboy";
+console.log(wolfy);
 //
