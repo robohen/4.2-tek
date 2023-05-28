5e89ba3cc3ce350795ce8259224c5111aae7934f
robohen <88636103+robohen@users.noreply.github.com>
2023-05-27 18:08:46 -0700

    Yell at the Ninja Turtles answered

--- lab-answers.js
+++ lab-answers.js
@@ -25,18 +25,24 @@
 // }
 //
 // Wild Wild Life//
-const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
-const sharky = ["Sharky", "shark", 20, "Left Coast"]
-const plantee = ["Plantee", "plant",  5000 , "Mordor"]
-const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
-const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+// const sharky = ["Sharky", "shark", 20, "Left Coast"]
+// const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+// const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
 
-plantee[2] = 5001;
-console.log(plantee);
-wolfy[3] = "Gotham City";
-console.log(wolfy);
-dart.push("Hawkins");
-console.log(dart);
-wolfy[0] = "Gameboy";
-console.log(wolfy);
+// plantee[2] = 5001;
+// console.log(plantee);
+// wolfy[3] = "Gotham City";
+// console.log(wolfy);
+// dart.push("Hawkins");
+// console.log(dart);
+// wolfy[0] = "Gameboy";
+// console.log(wolfy);
+//
+//Yell at the Ninja Turtles//
+const array =["Donatello", "Michaelangelo","Rafael", "Leonardo"];
+for (const element of array){
+    console.log(element.toUpperCase());
+}
 //
